e1336c7264e98889cad41184cbc39e0177765d90
minaripenguin <alexfinhart@gmail.com>
2022-07-16 08:35:11 +0800

    Launcher3: Add an option to customize blur radius
    * Set max blur radius to 175px
    
    [@neobuddy89: Add some clean up. Updated for A13 QPR1]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1041,6 +1041,7 @@ public final class Utilities {
         return isGSAEnabled(context) && LauncherPrefs.DOCK_MUSIC_SEARCH.get(context);
     }
 
+<<<<<<< HEAD
     public static boolean isResizeableActivity(Context context, ComponentName activity) {
         if (activity == null) return false;
         final ActivityInfo info;
@@ -1070,5 +1071,10 @@ public final class Utilities {
 
     public static void startLmoFreeform(Context context, ComponentName activity) {
         startLmoFreeform(context, activity, UserHandle.myUserId());
+
+    }
+
+    public static int getBlurRadius(Context context) {
+        return LauncherPrefs.BLUR_DEPTH.get(context);
     }
 }
