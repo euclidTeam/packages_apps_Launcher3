BashaMobile/android_packages_apps_Launcher3

[BUGFIX] fix hotseat showing behind recents bug
Change-Id: If7e2ebd287cc1af814ff132be8b424ac95bde9d4
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
@@ -64,6 +64,7 @@ import com.android.launcher3.states.StateAnimationConfig;
 import com.android.launcher3.touch.AllAppsSwipeController;
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 import com.android.launcher3.util.DisplayController;
+import com.android.launcher3.util.LauncherStatesHelper;
 import com.android.quickstep.util.RecentsAtomicAnimationFactory;
 import com.android.quickstep.util.SplitAnimationTimings;
 import com.android.quickstep.views.RecentsView;
@@ -96,6 +97,7 @@ public class QuickstepAtomicAnimationFactory extends
             StateAnimationConfig config) {
         RecentsView overview = mContainer.getOverviewPanel();
         boolean isPinnedTaskbar = DisplayController.isPinnedTaskbar(mContainer);
+        LauncherStatesHelper.setLauncherState(toState);
         if (toState == NORMAL) {
             com.android.launcher3.SettingsRepository.INSTANCE.get(overview.getContext()).onUserReturnedHome();
         }

euclidTeam/packages_apps_Launcher3

Signed-off-by: Dmitrii <bankersenator@gmail.com>

quickstep/src/com/android/launcher3/uioverrides/states/QuickstepAtomicAnimationFactory.java
@@ -64,6 +64,7 @@ import com.android.launcher3.states.StateAnimationConfig;
 import com.android.launcher3.touch.AllAppsSwipeController;
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 import com.android.launcher3.util.DisplayController;
+import com.android.launcher3.util.LauncherStatesHelper;
 import com.android.quickstep.util.RecentsAtomicAnimationFactory;
 import com.android.quickstep.util.SplitAnimationTimings;
 import com.android.quickstep.views.RecentsView;
@@ -96,6 +97,7 @@ public class QuickstepAtomicAnimationFactory extends
             StateAnimationConfig config) {
         RecentsView overview = mContainer.getOverviewPanel();
         boolean isPinnedTaskbar = DisplayController.isPinnedTaskbar(mContainer);
+        LauncherStatesHelper.setAnimationState(toState);
         if (toState == NORMAL) {
             com.android.launcher3.LauncherAppState.INSTANCE.executeIfCreated(app -> app.checkIfRestartNeeded());
         }