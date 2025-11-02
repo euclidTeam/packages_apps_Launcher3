Musical143143/android_packages_apps_Launcher3

[FEATURE]: add nothingOS always-on-display changes
Change-Id: Ie521beb9c76699f3626498cf1186423065e9bc9f

src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -237,6 +237,10 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
     public boolean onDoubleTap(MotionEvent event) {
         if (LauncherPrefs.getPrefs(mContext).getBoolean(SLEEP_GESTURE, true)) {
             VibratorWrapper.INSTANCE.get(mContext).vibrate(VibratorWrapper.EFFECT_CLICK);
+            com.android.quickstep.SystemUiProxy.INSTANCE.get(mContext).notifyGoingToSleepByDoubleClick(
+                (int) mTouchDownPoint.x,
+                (int) mTouchDownPoint.y
+            );
             mPm.goToSleep(event.getEventTime());
         }
         return true;

euclidTeam/packages_apps_Launcher3

src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -234,6 +234,10 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
         if (LauncherPrefs.SLEEP_GESTURE.get(context)) {
             if (LauncherPrefs.SLEEP_GESTURE_HAPTIC.get(context))
                 VibratorWrapper.INSTANCE.get(context).vibrate(VibratorWrapper.EFFECT_CLICK);
+                com.android.quickstep.SystemUiProxy.INSTANCE.get(context).notifyGoingToSleepByDoubleClick(
+                (int) mTouchDownPoint.x,
+                (int) mTouchDownPoint.y
+            );
             mPm.goToSleep(event.getEventTime());
         }
         return true;